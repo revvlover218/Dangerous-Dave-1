1c2b0cd62e87096a4f7c9c4b200c4da45fc38fda
revvlover218 <reshalluchman@yahoo.com>
2015-05-04 01:16:32 +0200

    added score text

--- Dave/Dave/main.cpp
+++ Dave/Dave/main.cpp
@@ -6,7 +6,7 @@
 
 //GLOBAL VARIABLES
 const int WIDTH = 1200;
-const int HEIGHT = 720;
+const int HEIGHT = 700;
 
 
 
@@ -29,8 +29,22 @@ int main(void)
 
 	if (!display)										//test display object
 		return -1;
-
+	//Initialization
 	al_init_primitives_addon();
+	al_init_font_addon();
+	al_init_ttf_addon();
+	
+	//Pointers
+	ALLEGRO_FONT *font24 = al_load_font("BAUHS93.TTF", 24, 0);
+	
+	//Clear screen to a black
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	al_draw_text(font24, al_map_rgb(0, 255, 0), 10, 5, 0, "Score:" );
+	al_flip_display();
+
+	al_rest(5.0);
+
 	man.InitDave(man);
 
 	while (!done)
@@ -40,6 +54,12 @@ int main(void)
 		al_flip_display();
 		al_clear_to_color(al_map_rgb(0, 0, 0));
 	}
+
+
+
+
+
+	//Destroying
 
 	al_destroy_display(display);						//destroy our display object
 
